26b8af4f35c0d88357255374cc38d72306660d43
Wassim <wawa.h8@gmail.com>
2019-03-09 21:43:48 +0100

    :lipstick: Added change handlers and profile saving

--- src/modules/Profile/Profile.js
+++ src/modules/Profile/Profile.js
@@ -4,23 +4,49 @@ import styles from './Profile.module.scss';
 class Profile extends Component {
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      currentUser: {}
+    };
   }
 
-  render() {
+  componentDidMount(){
     const currentUser = {
       "picture": "https://media.licdn.com/dms/image/C4D03AQEFEkHs6eyTKg/profile-displayphoto-shrink_200_200/0?e=1557360000&v=beta&t=iD77qK1-Gp3rGBetxiSJu6mu0E6wnCArxU2wKrtIUNI",
       "pseudo": "wawa.h8"
     }
+    this.setState({currentUser});
+  }
+
+  pictureChange = (e) => {
+    let files = e.target.files;
+    if (files && files[0]) {
+      let currentUser = this.state.currentUser;
+      currentUser.picture = files;
+      this.setState({files})
+    }
+  }
+
+  saveChanges = () => {
+    //PUT this.state.currentUser
+  }
+
+  render() {
     const {toggleProfile} = this.props;
+    const {currentUser} = this.state;
     return (
       <React.Fragment>
         <div className={styles["panel"]}>
           <h3>Your profile</h3>
           <img src={currentUser.picture} alt={currentUser.pseudo} title={currentUser.pseudo} style={{borderRadius: '100%'}}/>
+          <div className="custom-file mb-3">
+            <input type="file" className="custom-file-input" id="customFile" onChange={this.pictureChange}/>
+            <label className="custom-file-label" for="customFile">Default custom file input</label>
+          </div>
+
           <input type="text" className="form-control" value={currentUser.pseudo}/>
+
           <span>
-            <button className="btn btn-success">Save changes</button>
+            <button className="btn btn-success" onClick={this.saveChanges}>Save changes</button>
             <button className="btn btn-danger" onClick={toggleProfile}>Cancel</button>
           </span>
         </div>
