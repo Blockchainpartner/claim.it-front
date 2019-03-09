e1154ba908b3d52bd98e5a7fa5ba9e10164ad9ec
Wassim <wawa.h8@gmail.com>
2019-03-09 10:23:38 +0100

    :heavy_plus_sign: Added SCSS modules

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'shards-ui/dist/css/shards.min.css';
 
-import './App.scss'
+import style from './App.module.scss'
 
 class App extends Component {
   render() {
     return (
-      <div className="claimit-app">
+      <div className={style["claimit-app"]}>
         Welcome home
       </div>
     );
