b8b227db1a5a5f0cb715880fbf40954394c86b1f
Yosra Helal <helal.yosra@gmail.com>
2019-03-09 22:53:45 +0100

    ens front redux not working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'shards-ui/dist/css/shards.min.css';
 import {BrowserRouter, Route} from 'react-router-dom';
-
 import style from './App.module.scss';
 
 //Components
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {applyMiddleware, combineReducers, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ethReducer from "./reducers/EthReducer";
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * init store with contextual data from the API
+ */
+const reducer = combineReducers({
+    eth: ethReducer,
+});
+
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // TODO remove in Prod
+    applyMiddleware(thunk),
+);
+
+export {store};
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
