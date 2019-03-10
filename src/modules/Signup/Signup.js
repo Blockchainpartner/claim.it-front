8626534393f20969682b80ddde725aa7bbef1a29
Wassim <wawa.h8@gmail.com>
2019-03-10 02:07:11 +0100

    :heavy_plus_sign: Added and Implemented React Loader

--- src/modules/Signup/Signup.js
+++ src/modules/Signup/Signup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from './Signup.module.scss';
+import Loader from 'react-loader-spinner';
 
 //Components
 import HomeNav from '../../UI/HomeNav/HomeNav';
@@ -8,7 +9,8 @@ class Signup extends Component {
   constructor(props){
     super(props);
     this.state = {
-      avatar: undefined
+      avatar: undefined,
+      ready: true
     };
   }
 
@@ -25,21 +27,33 @@ class Signup extends Component {
   }
 
   render() {
-    let {avatar} = this.state;
-    return (
-      <React.Fragment>
-        <div className={styles["signup-panel"]}>
-          <HomeNav/>
-          <h2>Signup</h2>
-          <p>Easy signup! You just need to upload an avatar</p>
-          <div className={styles["file-input"]+" custom-file mb-3 card"}>
-            <input type="file" className="custom-file-input" id="customFile" onChange={this.pictureChange}/>
-            <label className="custom-file-label" for="customFile">{avatar ? avatar.name : 'Upload an avatar'}</label>
+    let {avatar, ready} = this.state;
+    if(!ready) {
+      return (
+        <React.Fragment>
+          <div className={styles["signup-panel"]}>
+            <HomeNav/>
+            <Loader type="Oval" color="green" height={80} width={80} />
           </div>
-          <button className="btn btn-success" onClick={this.signup}>Signup</button>
-        </div>
-      </React.Fragment>
-    );
+        </React.Fragment>
+      );
+    }
+    else {
+      return (
+        <React.Fragment>
+          <div className={styles["signup-panel"]}>
+            <HomeNav/>
+            <h2>Signup</h2>
+            <p>Easy signup! You just need to upload an avatar</p>
+            <div className={styles["file-input"]+" custom-file mb-3 card"}>
+              <input type="file" className="custom-file-input" id="customFile" onChange={this.pictureChange}/>
+              <label className="custom-file-label" for="customFile">{avatar ? avatar.name : 'Upload an avatar'}</label>
+            </div>
+            <button className="btn btn-success" onClick={this.signup}>Signup</button>
+          </div>
+        </React.Fragment>
+      );
+    }
   }
 }
 
