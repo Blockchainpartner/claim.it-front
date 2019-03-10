fb153163c85b27745536761fcd0d5b4e40778263
Wassim <wawa.h8@gmail.com>
2019-03-10 08:10:59 +0100

    :bug: Fixed logos with arweave

--- src/UI/BoardNav/BoardNav.js
+++ src/UI/BoardNav/BoardNav.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import styles from "./BoardNav.module.scss";
-import logo from "../../media/logo.png";
 
 //Components
 import SubProfile from './SubProfile';
 
 const BoardNav = (props) => {
   const {toggleProfile} = props;
+  const logo = 'https://arweave.net/M_dOEcRCYElOy1WXdoGpY8ITFAsid5dZyBQkmZwhvPk';
   return(
     <div className={styles["boardnav"]}>
       <Link to="/"><img src={logo} alt="claim.it"/></Link>
--- src/UI/HomeNav/HomeNav.js
+++ src/UI/HomeNav/HomeNav.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import styles from "./HomeNav.module.scss";
-import logo from "../../media/logo.png";
 
 const HomeNav = () => {
+  const logo = 'https://arweave.net/M_dOEcRCYElOy1WXdoGpY8ITFAsid5dZyBQkmZwhvPk';
   return(
     <div>
       <Link to="/"><img src={logo} alt="claim.it"/></Link>
